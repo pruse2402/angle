8e8353546f20fa915137dc18bd1233f78019ae2b
Purushotham <purus2895@gmail.com>
2020-04-04 21:08:45 +0530

    recover handler func has been implemented

---
+++ src/routes/middleware.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+// Prevent abnormal shutdown while panic
+func recoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				log.Println(string(debug.Stack()))
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
